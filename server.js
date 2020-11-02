0f4fde0e8d470b5f4eb5045ac125574b89d400e4
François Behague <behaque@nycticorax.org>
2020-11-02 12:46:54 +0100

    Finished Node.js crash course episode 5

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  
+  // lodash module
+  const num  = _.random(0, 20)
+  console.log(num)
+
+  const greet = _.once(() => {
+    console.log('hello');  
+  })
+
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
@@ -19,7 +29,7 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       break;
 
-      case '/about-me':
+      case '/about-blah':
       res.statusCode = 301;
       res.setHeader('Location', '/about');
       res.end();
@@ -27,8 +37,7 @@ const server = http.createServer((req, res) => {
 
       default:
         path += '404.html';
-        res.statusCode = 404;
-       
+        res.statusCode = 404;   
   }
 
   // send an html file
