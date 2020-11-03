9213eef7854cc0ff1a24b9db7812867c72635b04
François Behague <behaque@nycticorax.org>
2020-11-03 16:31:41 +0100

    Finished Node.js crash course tutorial episode 7

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ const { dirname } = require('path');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'lorem'},
+    {title: 'Mario finds stars', snippet: 'lorem'},
+    {title: 'How to defeat bowser', snippet: 'lorem'},
+  ];
+  res.render('index', { title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About'});
 });
 
-// redirects
-app.get('/about-us', (req,res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req,res) => {
+  res.render('create', { title: 'Create a new Blog'});
 })
-
 
 
 // 404 page has to be at the bottom
 app.use((req,res) => {
-  res.status(404).res.sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404', { title: '404'})
 })
