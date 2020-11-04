c0db3474662e51adf8ae45d08a48cad54674f084
François Behague <behaque@nycticorax.org>
2020-11-04 21:22:47 +0100

    Finished node js crash course tutorial episode 9

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const { dirname } = require('path');
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+const { result } = require('lodash');
+const Blog = require('./models/blog')
 
 // express app
 const app = express();
 
+// connect to mongo db
+const dbURI = 'mongodb+srv://frivoc:fakepasswordforgityouknow@nodejs.dwwyh.mongodb.net/<node-tutorial>?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files 
 app.use(express.static('public'))
 app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'lorem'},
-    {title: 'Mario finds stars', snippet: 'lorem'},
-    {title: 'How to defeat bowser', snippet: 'lorem'},
-  ];
-  res.render('index', { title: 'Home', blogs: blogs});
+res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About'});
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+  .then((result) => {
+res.render('index', { title: 'All Blogs', blogs: result})
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
 
 app.get('/blogs/create', (req,res) => {
   res.render('create', { title: 'Create a new Blog'});
