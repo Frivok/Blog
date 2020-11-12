588a30348059159606eb7d20b1d19ed2df1de39e
François Behague <behaque@nycticorax.org>
2020-11-12 14:40:33 +0100

    Finished node.js crash course episode 10

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const { dirname } = require('path');
 const morgan = require('morgan')
 const mongoose = require('mongoose');
 const { result } = require('lodash');
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // express app
 const app = express();
 
 // connect to mongo db
-const dbURI = 'mongodb+srv://frivoc:fakepasswordforgityouknow@nodejs.dwwyh.mongodb.net/<node-tutorial>?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://frivoc:fakepasswordforgit@nodejs.dwwyh.mongodb.net/<node-tutorial>?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err))
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'));
 
 // routes
@@ -41,11 +43,44 @@ res.render('index', { title: 'All Blogs', blogs: result})
   })
 })
 
+app.post('/blogs', (req, res) => {
+const blog = new Blog(req.body)
+
+blog.save()
+.then(result => {
+  res.redirect('/blogs')
+})
+.catch(err => {
+  console.log(err)
+})
+})
+
 app.get('/blogs/create', (req,res) => {
   res.render('create', { title: 'Create a new Blog'});
 })
 
+app.get('/blogs/:id', (req, res) => {
+const id = req.params.id;
+Blog.findById(id)
+.then(result => {
+  res.render('details', { blog: result, title: 'Blog Details'})
+})
+.catch(err => {
+  console.log(err)
+})
+})
 
+app.delete('/blogs/:id', (req,res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+  .then(result => {
+    res.json({ redirect: '/blogs' })
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
 // 404 page has to be at the bottom
 app.use((req,res) => {
   res.status(404).render('404', { title: '404'})
