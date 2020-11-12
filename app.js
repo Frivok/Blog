e92fcccb592aa3b3e0a89d7472f13ab6500d8c03
François Behague <behaque@nycticorax.org>
2020-11-12 20:50:35 +0100

    Finished node.js crash course tutorial episode 11

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const { dirname } = require('path');
-const morgan = require('morgan')
+const express = require('express')
+const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { result } = require('lodash');
-const Blog = require('./models/blog');
-const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
 
 // connect to mongo db
 const dbURI = 'mongodb+srv://frivoc:fakepasswordforgit@nodejs.dwwyh.mongodb.net/<node-tutorial>?retryWrites=true&w=majority';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err))
@@ -32,55 +30,9 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About'});
 });
 
-// blog routes
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-  .then((result) => {
-res.render('index', { title: 'All Blogs', blogs: result})
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-})
+//blog routes
+app.use('/blogs/', blogRoutes);
 
-app.post('/blogs', (req, res) => {
-const blog = new Blog(req.body)
-
-blog.save()
-.then(result => {
-  res.redirect('/blogs')
-})
-.catch(err => {
-  console.log(err)
-})
-})
-
-app.get('/blogs/create', (req,res) => {
-  res.render('create', { title: 'Create a new Blog'});
-})
-
-app.get('/blogs/:id', (req, res) => {
-const id = req.params.id;
-Blog.findById(id)
-.then(result => {
-  res.render('details', { blog: result, title: 'Blog Details'})
-})
-.catch(err => {
-  console.log(err)
-})
-})
-
-app.delete('/blogs/:id', (req,res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-  .then(result => {
-    res.json({ redirect: '/blogs' })
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
 // 404 page has to be at the bottom
 app.use((req,res) => {
   res.status(404).render('404', { title: '404'})
